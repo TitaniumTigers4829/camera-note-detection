65b963c101737c1fbff01076cdba8ac6522ee5dc
jgelia <jgelia@ncsu.edu>
2024-02-14 17:45:55 -0500

    Added in network table comms

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from networktables import NetworkTablesInstance
+from networktables import NetworkTables
 
 
 # Specify the camera index (usually 0 for built-in webcam)
@@ -42,6 +42,11 @@ def find_largest_orange_clump(image):
     return None, None
 
 
+# Connects to the robot
+# TODO: Figure out what this address should be, I think it's this or maybe roborio-4829-frc.local
+NetworkTables.initialize(server="127.0.0.1")
+smart_dashboard = NetworkTables.getTable("SmartDashboard")
+
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -53,14 +58,16 @@ while True:
         if position is not None and size is not None:
             x, y = position
             w, h = size
-
             # Stops it from detecting disks partially offscreen
             # TODO: Once we have a fixed camera angle, we could probably find a ratio between disk width and height
             if (x > detection_padding and y > detection_padding and
                     x + w < frame.shape[1] - detection_padding and y + h < frame.shape[0] - detection_padding):
-
                 # Draw rectangle around the largest clump
                 cv2.rectangle(frame, position, (x + w, y + h), (0, 255, 0), 2)
+            # TODO: Add in code to send values about the notes it sees here
+            smart_dashboard.putBoolean("Can See Note", True)
+        else:
+            smart_dashboard.putBoolean("Can See Note", False)
 
         # Display the resulting frame
         cv2.imshow("Frame", frame)
