1144f074d7078849947529a31db30b9bfccf24a1
Jack <85963782+JacksonElia@users.noreply.github.com>
2024-02-18 23:04:56 -0500

    Update calibrate_bounds.py

--- calibrate_bounds.py
+++ calibrate_bounds.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 
+# This isn't for the detection, it's just to help you find the lower and upper hsv bounds.
+# Green represents a contour that has been found, blue represents the biggest contour. Try to get it so that they
+# only appear around notes.
+
 # Create a window to adjust the lower and upper bounds
 cv2.namedWindow("Trackbars", cv2.WINDOW_NORMAL)  # Use WINDOW_NORMAL to allow resizing
 cv2.resizeWindow("Trackbars", 600, 300)  # Set the size of the window (width, height)
